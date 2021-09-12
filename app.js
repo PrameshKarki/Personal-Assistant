a7b1222d70d85f5538dc6e116e53ada2943e1659
Pramesh Karki <Parameswarkarki@gmail.com>
2021-09-12 22:18:30 +0545

    Ïnitial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// *Import require modules
+const path=require("path");
+
+const notifier=require("node-notifier");
+const schedule=require("node-schedule");
+
+// *JOB Definition
+const job=schedule.scheduleJob("*/1 * * * *",()=>{
+      notifier.notify({
+        title:"Hi Pramesh,Time to Drink Water",
+        message:"Drinking water is very important to keep our body hydrated,so please keep drinking water.Water is very important for our body.",
+        icon: path.join(__dirname,"images","water.jpg"),
+        sound:true,
+        wait:true
+
+      })
+});
